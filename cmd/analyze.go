30e392fba9c05cd4f2c806d81e5da81a6c4dd6b0
Arsh Sharma <arshsharma461@gmail.com>
2021-03-04 15:18:24 +0530

    feat: exporting json file
    Signed-off-by: Arsh Sharma <arshsharma461@gmail.com>

--- cmd/analyze.go
+++ cmd/analyze.go
@@ -1,17 +1,17 @@
 package cmd
 
 import (
-	"fmt"
-	"log"
+	"encoding/json"
+	"io/ioutil"
 	"os/exec"
 	"strings"
 
 	"github.com/spf13/cobra"
 )
 
-// analyzeCmd represents the analyze command
-var analyzeCmd = &cobra.Command{
-	Use:   "analyze",
+// totalDepCmd represents the totalDep command
+var totalDepCmd = &cobra.Command{
+	Use:   "totalDep",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -19,36 +19,45 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("analyze called")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		//fmt.Println(args)
+		// TODO: allow taking an arg and running analysis in that dir
 		totalDepCmd := exec.Command("go", "list", "-m", "all")
-		// totalDepCmd.Stdout = os.Stdout
-		// totalDepCmd.Stderr = os.Stdout
-		// if err := totalDepCmd.Run(); err != nil {
-		// 	log.Fatalln("Error: ", err)
-		// }
+
 		output, err := totalDepCmd.Output()
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 		outputString := string(output)
 		totalDeps := strings.Count(outputString, "\n") - 1
 
-		fmt.Println("Total Dependencies: ", totalDeps)
-
+		outputObj := struct {
+			SA int `json:"totalDependencies"`
+		}{
+			SA: totalDeps,
+		}
+		outputRaw, err := json.Marshal(outputObj)
+		if err != nil {
+			return err
+		}
+		err = ioutil.WriteFile("analysis.json", outputRaw, 0644)
+		if err != nil {
+			return err
+		}
+		return nil
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(analyzeCmd)
+	rootCmd.AddCommand(totalDepCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// analyzeCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// totalDepCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// analyzeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// totalDepCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
