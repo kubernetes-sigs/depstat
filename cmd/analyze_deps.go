e2b291764af1a13f989a48ae7e40e911a3391ce2
Arsh Sharma <arshsharma461@gmail.com>
2021-03-12 18:45:30 +0530

    docs: typo
    Signed-off-by: Arsh Sharma <arshsharma461@gmail.com>

--- cmd/analyze_deps.go
+++ cmd/analyze_deps.go
@@ -22,7 +22,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("analyzeDeps calledss")
+		fmt.Println("analyzeDeps called")
 
 		// get flags
 		verbose, err := cmd.Flags().GetBool("verbose")
