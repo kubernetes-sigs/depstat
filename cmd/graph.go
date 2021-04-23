74252eb40d46b1998aad6f1b4aab5e2f7d478bd6
RinkiyaKeDad <arshsharma461@gmail.com>
2021-04-23 17:12:23 +0530

    initial commit
    Signed-off-by: RinkiyaKeDad <arshsharma461@gmail.com>

--- cmd/graph.go
+++ cmd/graph.go
@@ -23,6 +23,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var dep string
+
 var graphCmd = &cobra.Command{
 	Use:   "graph",
 	Short: "Generate a .dot file to be used with Graphviz's dot command.",
@@ -30,15 +32,59 @@ var graphCmd = &cobra.Command{
 	For example to generate a svg image use:
 	twopi -Tsvg -o dag.svg graph.dot`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		depGraph, deps, _ := getDepInfo()
-		fileContents := "digraph {\ngraph [rankdir=TB, overlap=false];\n"
-		for _, dep := range deps {
-			_, ok := depGraph[dep]
-			if !ok {
-				continue
+		depGraph, deps, mainModule := getDepInfo()
+		// strict ensures that there is only one edge between two vertices
+		// overlap = false ensures the vertices don't overlap
+		fileContents := "strict digraph {\ngraph [overlap=false];\n"
+
+		// graph to be generated is based around input dep
+		if dep != "" {
+			var cycleChains []Chain
+			var chains []Chain
+			var temp Chain
+			getChains(mainModule, depGraph, temp, &chains, &cycleChains)
+			// to color the entered node as yellow
+			fileContents += fmt.Sprintf("MainNode [label=\"%s\", style=\"filled\" color=\"yellow\"]", dep)
+
+			// add all chains which have the input dep to the .dot file
+			for _, chain := range chains {
+				if chainContains(chain, dep) {
+					for i, d := range chain {
+						if i == len(chain)-1 {
+							if d == dep {
+								// for the input dep use a colored node
+								fileContents += "MainNode\n"
+							} else {
+								fileContents += fmt.Sprintf("\"%s\"\n", d)
+							}
+						} else {
+							if d == dep {
+								// for the input dep use a colored node
+								fileContents += "MainNode -> "
+							} else {
+								fileContents += fmt.Sprintf("\"%s\" -> ", d)
+							}
+						}
+					}
+				}
 			}
-			for _, neighbour := range depGraph[dep] {
-				fileContents += fmt.Sprintf("\"%s\" -> \"%s\"\n", dep, neighbour)
+		} else {
+			// color the main module as yellow
+			fileContents += fmt.Sprintf("MainNode [label=\"%s\", style=\"filled\" color=\"yellow\"]", mainModule)
+			for _, dep := range deps {
+				_, ok := depGraph[dep]
+				if !ok {
+					continue
+				}
+				// main module can never be a neighbour
+				for _, neighbour := range depGraph[dep] {
+					if dep == mainModule {
+						// for the main module use a colored node
+						fileContents += fmt.Sprintf("\"MainNode\" -> \"%s\"\n", neighbour)
+					} else {
+						fileContents += fmt.Sprintf("\"%s\" -> \"%s\"\n", dep, neighbour)
+					}
+				}
 			}
 		}
 		fileContents += "}"
@@ -52,6 +98,16 @@ var graphCmd = &cobra.Command{
 	},
 }
 
+func chainContains(chain Chain, dep string) bool {
+	for _, d := range chain {
+		if d == dep {
+			return true
+		}
+	}
+	return false
+}
+
 func init() {
 	rootCmd.AddCommand(graphCmd)
+	graphCmd.Flags().StringVarP(&dep, "dep", "d", "", "Specify dependency to create a graph around")
 }
